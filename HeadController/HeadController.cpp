8937796bcc529f948df09ca9de22471042fe1504
DongGyuLee <49723556+Doroco@users.noreply.github.com>
2023-04-17 14:01:40 +0900

    운용통제기 컨트로 클레스 업데이트
    운용통제기 업데이트 클래스 업데이트

---
+++ HeadController/HeadController.cpp
@@ -0,0 +1,19 @@
+﻿// HeadController.cpp : 정적 라이브러리를 위한 함수를 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "HeadController.h"
+
+// TODO: 라이브러리 함수의 예제입니다.
+void fnHeadController()
+{
+}
+
+void HeadController::setMissleScenario(int _sx, int _sy)
+{
+}
+
+void HeadController::setTargetScenario(int _sx, int _sy, int _ex, int _ey, char _kind, double _speed)
+{
+}
---
+++ HeadController/ScenarioSetting.h
@@ -0,0 +1,37 @@
+#pragma once
+
+//시나리오 입력
+//class ScenarioSetting {
+//private:
+//	Missile missile;
+//	Target target;
+//
+//public:
+//	void SetMissile(double cord[2], double range);
+//	void SetTarget(double cord[4], char kind, double speed);
+//	Missile GetMissile();
+//	Target GetTarget();
+//};
+//
+////유도탄 시작 지점 x,y 좌표
+//class Missile {
+//private:
+//	double _x, _y;
+//	double _range;
+//
+//public:
+//	void SetMissile(double coordinate[2], double range);
+//};
+//
+////공중위협 시작 지점 x,y 좌표
+////공중위협 종료 지점 x,y 좌표
+//class Target {
+//private:
+//	double _sx, _sy;
+//	double _ex, _ey;
+//	char _kind;
+//	double _speed;
+//
+//public:
+//	void SetTarget(double coordinate[4], char kind, double speed);
+//};
