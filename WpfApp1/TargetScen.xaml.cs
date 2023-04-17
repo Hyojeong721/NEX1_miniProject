4d5879970156ec331915dc3077b3de38e8f81716
hyojeong jang <ase0574@naver.com>
2023-04-17 14:10:40 +0900

    [시나리오 설정 팝업창 생성]
    시나리오 설정을 위한 팝업창 생성중입니다.

--- WpfApp1/TargetScen.xaml.cs
+++ WpfApp1/TargetScen.xaml.cs
@@ -10,30 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// TargetScen.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TargetScen : Window
     {
-        public MainWindow()
+        public TargetScen()
         {
             InitializeComponent();
-            WindowStyle = WindowStyle.None;
-            ResizeMode = ResizeMode.NoResize;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Window targetScen = new TargetScen();
-            targetScen.Owner = Application.Current.MainWindow;
-            targetScen.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-            targetScen.ShowDialog();
-        }
 
+        }
     }
 }
